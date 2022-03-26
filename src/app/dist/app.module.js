56ab811512bdb6148b8e7b87c6f8b81781432437
MewadaChirag <mewada.chirag@gmail.com>
2022-03-26 15:01:18 +0530

    first push

---
+++ src/app/dist/app.module.js
@@ -0,0 +1,39 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+exports.__esModule = true;
+exports.AppModule = void 0;
+var core_1 = require("@angular/core");
+var platform_browser_1 = require("@angular/platform-browser");
+var app_component_1 = require("./app.component");
+var inspection_component_1 = require("./inspection/inspection.component");
+var show_inspection_component_1 = require("./inspection/show-inspection/show-inspection.component");
+var add_edit_inspection_component_1 = require("./inspection/add-edit-inspection/add-edit-inspection.component");
+var AppModule = /** @class */ (function () {
+    function AppModule() {
+    }
+    AppModule = __decorate([
+        core_1.NgModule({
+            declarations: [
+                app_component_1.AppComponent,
+                inspection_component_1.InspectionComponent,
+                show_inspection_component_1.ShowInspectionComponent,
+                add_edit_inspection_component_1.AddEditInspectionComponent
+            ],
+            imports: [
+                platform_browser_1.BrowserModule,
+                HttpClientModule,
+                FormsModule,
+                ReactiveFormsModule
+            ],
+            providers: [InspectionApiService],
+            bootstrap: [app_component_1.AppComponent]
+        })
+    ], AppModule);
+    return AppModule;
+}());
+exports.AppModule = AppModule;
